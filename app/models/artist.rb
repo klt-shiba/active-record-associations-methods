d007b484fa0208277412929efe2197f364eca17b
Colin Tseng <klt.aus@yahoo.com>
2021-08-08 17:59:41 +1000

    Completed assignment

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -3,14 +3,26 @@ class Artist < ActiveRecord::Base
   has_many :genres, through: :songs
 
   def get_genre_of_first_song
-    #return the genre of the artist's first saved song
+    # Store value of first song genre id
+    genre_id = self.songs.first.genre_id
+
+    #Search Genre object for genre id
+    Genre.find(genre_id) 
   end
 
   def song_count
-    #return the number of songs associated with the artist
+
+    # Grab songs and count the number of items in array
+    self.songs.length
   end
 
   def genre_count
+
+    results = ""
+    self.songs.each do |song| 
+      results = song.genre_id
+    end
+    results
     #return the number of genres associated with the artist
   end
 end
